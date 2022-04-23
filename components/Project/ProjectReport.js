ff033894248a87e00ba5883edfe4c2641b8dfd6a
Jimmy McBroom <james.mcbroom@gmail.com>
2022-04-23 18:53:24 +0200

    update submit form

--- components/Project/ProjectReport.js
+++ components/Project/ProjectReport.js
@@ -1,67 +1,137 @@
 import { useState } from "react";
 import SiteSection from "../../components/SiteSection";
-import { supabase } from '../../utils/supabaseClient'
+import { supabase } from "../../utils/supabaseClient";
 
 const ProjectReport = ({ id }) => {
-
   const handleClick = (queryString, photoUrl, setResponse) => {
-
-    if(photoUrl) {
-      queryString.Attachments = JSON.stringify([{ url: photoUrl }])
+    if (photoUrl) {
+      queryString.Attachments = JSON.stringify([{ url: photoUrl }]);
     }
-    
-    fetch(`/api/createReport?${new URLSearchParams(queryString).toString()}`)
-      .then(r => r.json())
-      .then(d => setResponse(d))
-  }
 
-  const [photoUrl, setPhotoUrl] = useState('')
+    fetch(`/api/createReport?${new URLSearchParams(queryString).toString()}`)
+      .then((r) => r.json())
+      .then((d) => setResponse(d));
+  };
+
+  const [photoUrl, setPhotoUrl] = useState("");
 
   async function handleUpload(e) {
-    const avatarFile = e.target.files[0]
-    const { data, error } = await supabase
-      .storage
-      .from('report-photos')
+    const avatarFile = e.target.files[0];
+    const { data, error } = await supabase.storage
+      .from("report-photos")
       .upload(avatarFile.name, avatarFile, {
-        cacheControl: '3600',
-        upsert: false
-      })
-    setPhotoUrl(`https://zikieyzmibovmrmmdxso.supabase.in/storage/v1/object/public/${data.Key}`)
+        cacheControl: "3600",
+        upsert: false,
+      });
+    setPhotoUrl(
+      `https://zikieyzmibovmrmmdxso.supabase.in/storage/v1/object/public/${data.Key}`
+    );
   }
 
   const [record, setRecord] = useState({
-    'Report': '',
-    'Email': '',
-    'Projects': id
-  })
+    Report: "",
+    Email: "",
+    Projects: id,
+    Name: "",
+    "Publish Photo": null,
+  });
 
-  const [response, setResponse] = useState(null)
+  const [response, setResponse] = useState(null);
 
   return (
     <div className="max-w-xl submit-form mx-auto">
       <h2 className="mb-9">Tell us what you know about this project</h2>
       <div className="flex flex-col mb-2 border">
         <label htmlFor="contact">What do you see?</label>
-        <textarea value={record.Report} rows={4} className="w-full" onChange={e => setRecord({ ...record, Report: e.target.value })} />
+        <textarea
+          value={record.Report}
+          rows={4}
+          className="w-full"
+          onChange={(e) => setRecord({ ...record, Report: e.target.value })}
+        />
       </div>
       <div>
         <label htmlFor="photoupload">Upload a photo (optional)</label>
-        <input type="file" id="photoupload" onChange={handleUpload} className="p-4 py-12" />
+        <input
+          type="file"
+          id="photoupload"
+          onChange={handleUpload}
+          className="p-4 py-12"
+        />
       </div>
       <div className="flex flex-col mb-2 border">
         <label htmlFor="contact">What&apos;s your email? (optional)</label>
-        <input type="text" value={record.Email} name="contact" id="contact" onChange={e => setRecord({ ...record, Email: e.target.value })} />
+        <input
+          type="text"
+          value={record.Email}
+          name="contact"
+          id="contact"
+          onChange={(e) => setRecord({ ...record, Email: e.target.value })}
+        />
       </div>
+      <div>
+        <label htmlFor="textarea">What&apos;s your name? </label>
+        <input
+          type="text"
+          placeholder=""
+          value={record.Name}
+          onChange={(e) => setRecord({ ...record, Name: e.target.value })}
+        />
+      </div>
+      <div>
+          <label htmlFor="textarea">What is the photo&apos;s source? </label>
+          <radiogroup className="flex flex-col gap-2">
+            <div className="flex items-center flex-row">
+              <input
+                type="radio"
+                id="yespublish"
+                checked={record["Publish Photo"] === 'Yes'}
+                onClick={(e) => setRecord({ ...record, "Publish Photo": 'Yes' })}
+                className="inline"
+              />
+              <label htmlFor="yespublish" className="inline ml-3 nostyle">
+                I took this photo and you can publish it in the tracker.
+              </label>
+            </div>
+            <div className="flex items-center flex-row">
+              <input
+                type="radio"
+                id="nopublish"
+                className="inline"
+                checked={record["Publish Photo"] === 'No'}
+                onClick={(e) => setRecord({ ...record, "Publish Photo": 'No' })}
+              />
+              <label htmlFor="nopublish" className="inline ml-3 nostyle">
+                I found this photo elsewhere or don&apos;t want it to be
+                published.
+              </label>
+            </div>
+          </radiogroup>
+        </div>
       <div className="flex items-center justify-around">
-        {!response && <button onClick={() => handleClick(record, photoUrl, setResponse)} disabled={response !== null} className="my-3 mx-auto w-32">{"Submit"}</button>}
-        {response && <span
-          className="w-full flex justify-around items-center py-8 text-lg leading-6"
-          style={{ background: `rgba(215, 226, 255, 0.4)`, fontFamily: "DM Sans" }}>
-          Thanks! We received your tip.
-        </span>}
+        {!response && (
+          <button
+            onClick={() => handleClick(record, photoUrl, setResponse)}
+            disabled={response !== null}
+            className="my-3 mx-auto w-32"
+          >
+            {"Submit"}
+          </button>
+        )}
+        {response && (
+          <span
+            className="w-full flex justify-around items-center py-8 text-lg leading-6"
+            style={{
+              background: `rgba(215, 226, 255, 0.4)`,
+              fontFamily: "DM Sans",
+            }}
+          >
+            Thanks! We received your tip.
+          </span>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default ProjectReport;
--- pages/submit-a-tip.js
+++ pages/submit-a-tip.js
@@ -1,45 +1,53 @@
-import { useState } from "react"
-import { supabase } from '../utils/supabaseClient'
-import Head from 'next/head'
-import { siteTitle } from '../toolkit.config'
+import { useState } from "react";
+import { supabase } from "../utils/supabaseClient";
+import Head from "next/head";
+import { siteTitle } from "../toolkit.config";
 
 const handleClick = (queryString, setResponse) => {
   fetch(`/api/createReport?${new URLSearchParams(queryString).toString()}`)
-    .then(r => r.json())
-    .then(d => setResponse(d))
-}
+    .then((r) => r.json())
+    .then((d) => setResponse(d));
+};
 
 export default function SubmitPage() {
-
-  const [photoUrl, setPhotoUrl] = useState('')
-  const [response, setResponse] = useState(null)
+  const [photoUrl, setPhotoUrl] = useState("");
+  const [response, setResponse] = useState(null);
 
   async function handleUpload(e) {
-    const reportFile = e.target.files[0]
-    const { data, error } = await supabase
-      .storage
-      .from('report-photos')
+    const reportFile = e.target.files[0];
+    const { data, error } = await supabase.storage
+      .from("report-photos")
       .upload(reportFile.name, reportFile, {
-        cacheControl: '3600',
-        upsert: false
-      })
-    setPhotoUrl(`${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/${data.Key}`)
+        cacheControl: "3600",
+        upsert: false,
+      });
+    setPhotoUrl(
+      `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/${data.Key}`
+    );
   }
 
-  const [address, setAddress] = useState('')
-  const [report, setReport] = useState('')
-  const [email, setEmail] = useState('')
+  const [address, setAddress] = useState("");
+  const [report, setReport] = useState("");
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [publishPhoto, setPublishPhoto] = useState(null);
 
   let record = {
     Address: address,
     Report: report,
-    Email: email
+    Email: email,
+  };
+
+  if (publishPhoto === true) {
+    record["Publish photo?"] = "Yes";
+  }
+  if (publishPhoto === false) {
+    record["Publish photo?"] = "No";
   }
 
-  if (photoUrl !== '') {
-    record.Attachments = JSON.stringify([{ url: photoUrl }])
+  if (photoUrl !== "") {
+    record.Attachments = JSON.stringify([{ url: photoUrl }]);
   }
-
 
   return (
     <>
@@ -52,47 +60,115 @@ export default function SubmitPage() {
           key="description"
         />
         <meta property="og:title" content={siteTitle} key="title" />
-        <meta property="og:description" content="Use the Detroit Development Tracker to look up information about real estate development in the city." />
+        <meta
+          property="og:description"
+          content="Use the Detroit Development Tracker to look up information about real estate development in the city."
+        />
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
 
       <div className="max-w-xl mx-auto submit-form">
         <h2>Let us know what you&apos;re seeing.</h2>
-        <p className="pb-5 md:pb-11 leading-6">Do you see a project in your neighborhood or development activity? We&apos;ll check it out and get back to you.</p>
+        <p className="pb-5 md:pb-11 leading-6">
+          Do you see a project in your neighborhood or development activity?
+          We&apos;ll check it out and get back to you.
+        </p>
         <div>
           <label htmlFor="textarea">Where are you?</label>
-          <input type="text" value={address} placeholder="Type an address or intersection." onChange={(e) => setAddress(e.target.value)} />
+          <input
+            type="text"
+            value={address}
+            placeholder="Type an address or intersection."
+            onChange={(e) => setAddress(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="textarea">What do you see?</label>
-          <textarea cols={80} rows={5} value={report} onChange={(e) => setReport(e.target.value)} />
+          <textarea
+            cols={80}
+            rows={5}
+            value={report}
+            onChange={(e) => setReport(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor="photoupload">Upload a photo</label>
-          <input type="file" id="photoupload" onChange={handleUpload} className="p-4 py-12" />
+          <input
+            type="file"
+            id="photoupload"
+            onChange={handleUpload}
+            className="p-4 py-12"
+          />
         </div>
         <div>
-          <label htmlFor="textarea">What&apos;s your email? (optional)</label>
-          <input type="text" value={email} placeholder="" onChange={(e) => setEmail(e.target.value)} />
+          <label htmlFor="textarea">What is the photo&apos;s source? </label>
+          <radiogroup className="flex flex-col gap-2">
+            <div className="flex items-center flex-row">
+              <input
+                type="radio"
+                id="yespublish"
+                checked={publishPhoto === true}
+                onClick={() => setPublishPhoto(true)}
+                className="inline"
+              />
+              <label htmlFor="yespublish" className="inline ml-3 nostyle">
+                I took this photo and you can publish it in the tracker.
+              </label>
+            </div>
+            <div className="flex items-center flex-row">
+              <input
+                type="radio"
+                id="nopublish"
+                className="inline"
+                checked={publishPhoto === false}
+                onClick={() => setPublishPhoto(false)}
+              />
+              <label htmlFor="nopublish" className="inline ml-3 nostyle">
+                I found this photo elsewhere or don&apos;t want it to be
+                published.
+              </label>
+            </div>
+          </radiogroup>
+        </div>
+        <div>
+          <label htmlFor="textarea">What&apos;s your name? </label>
+          <input
+            type="text"
+            placeholder=""
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="textarea">What&apos;s your email?</label>
+          <input
+            type="text"
+            value={email}
+            placeholder=""
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <article className="flex items-end">
-
-          {!response ?
+          {!response ? (
             <button
               onClick={() => handleClick(record, setResponse)}
-              disabled={response !== null} >
+              disabled={response !== null}
+            >
               {"Submit"}
             </button>
-            :
+          ) : (
             <span
               className="w-full flex justify-around items-center py-8 text-lg leading-6"
-              style={{ background: `rgba(215, 226, 255, 0.4)`, fontFamily: "DM Sans" }}>
+              style={{
+                background: `rgba(215, 226, 255, 0.4)`,
+                fontFamily: "DM Sans",
+              }}
+            >
               Thanks! We received your tip.
             </span>
-          }
-
+          )}
         </article>
       </div>
     </>
-  )
+  );
 }
